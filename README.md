8de2ef5b3db72c3464100991ef10b5ab20f8334b
Matthew Spence <matthew.spence@clock-work.co.uk>
2017-09-04 11:05:31 +0100

    Copy local repository

---
+++ README.md
@@ -0,0 +1,33 @@
+# Critical Css Gen
+
+A neat tunnel into Penthouse, used to generate critical css.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+	*	Node.js
+
+### Installing
+
+A small step-by-step guide to setting up this project:
+
+Install mode_modules:	`npm install`
+
+Open up gulpfile.js, and enter the correct details of your target. An example of this can be found under the `/test` directory.
+
+
+## Using
+
+Enter a command prompt, and navigate to the root directory of this installation. Assuming you have configured the target inside `gulpfile.js`, you can proceed with the following command:
+
+`gulp`
+
+Here you will be presented with a prompt to enter the URL of the target, and then asked where to output the final result. Shouldn't need to change these details, and can simply confirm the default values set inside `gulpfile.js` by hitting enter.
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+//################################################################################################
+//	Optional Variables
+//################################################################################################
+
+const default_uri = "";	//can be remote...
+
+var css_files = [	//must be local. More than one css file is allowed.
+	""
+];
+
+const browser = {
+	dimensions: {
+		width: 1300,
+		height: 900
+	},
+	strict: false,
+	renderWaitTime: 100,
+	blockjs: true,
+};
+
+//################################################################################################
+//	End of options
+//################################################################################################
+
+
+
+
+const $ = require('gulp-load-plugins')({
+    pattern: ['*'],
+    scope: ['devDependencies']
+});
+const pkg = require('./package.json');
+$.util.log = function(){return this;};
+
+$.gulp.task("default",function(){
+	setTimeout(function(){
+		$.clear();
+		$.inquirer.prompt([
+			{
+				type: "input",
+				name: "uri",
+				message: "hostname or relative html file:\n",
+				default: default_uri
+			}
+			]).then(function(a){
+				if (a.url!=""){
+					var uri = a.uri;
+					$.clear();
+					$.inquirer.prompt([
+					{
+						type: "input",
+						name: "output",
+						message: "Output file: location/file.css",
+						default: "./outfile.css"	
+					}
+					]).then(function(a){
+						$.clear();
+						if (a.output!=""){
+							var outputFile = a.output.replace(/['"]+/g, '').trim();
+							$.fancyLog("Clearing any previous outfile file...");
+							$.fs.truncate(outputFile,"",function(){
+								$.fancyLog("Cleared file.");
+								for (var i = 0; i < css_files.length; i++){
+									$.fs.access(css_files[i],(err) => {
+										if (err){
+											$.fancyLog("File "+css_files[i]+" cannot be found!");
+											css_files = css_files.splice(i,1);
+										}
+									});
+									$.fancyLog("Added CSS File: "+css_files[i]);
+									var file = css_files[i];
+									var l = css_files.length;
+									$.penthouse({
+										url: uri,
+										css: file,
+										width: browser.dimensions.width,
+										height: browser.dimensions.height,
+										forceInclude: [],
+										timeout: 30000,
+										strict: browser.strict,
+										maxEmbeddedBase64Length: 1000,
+										userAgent: "Penthouse Critical Path CSS Generator",
+										renderWaitTime: browser.renderWaitTime,
+										blockJsRequests: browser.blockjs,
+										phantomJsOptions: {
+											//
+										},
+										customPageHeaders: {
+											"Accept-Encoding":"identity"
+										}
+									})
+										.then(criticalCss => {
+											$.fancyLog("Appending to "+outputFile);
+											$.fs.appendFileSync(outputFile,criticalCss);
+											if (i==l){
+												$.fancyLog("Completed.");
+											}
+										})
+										.catch(err => {
+											$.fancyLog("Error: "+err);
+										});
+								}
+							});
+						}
+					});
+				}
+				else {
+					$.fancyLog("No URL entered!");
+					return;
+				}
+			})
+	},1000);
+});
